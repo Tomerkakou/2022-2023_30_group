bc7303d192d717e5bffe566545bacaadc7c2123e
Tomerkakou <117356319+Tomerkakou@users.noreply.github.com>
2022-12-05 11:41:29 +0200

    change student style and inventory

--- wms/worker/urls.py
+++ wms/worker/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from worker.views import menu,new_inventory
+from worker.views import menu,inventory_receipt
 from website.views import start
 
 urlpatterns = [
     path('',start,name='login'),
     path('worker/menu/',menu,name='menu'),
-    path('worker/menu/new_inventory/',new_inventory,name='newinventory')
+    path('worker/menu/new_inventory/',inventory_receipt,name='newinventory')
 ]
--- wms/worker/views.py
+++ wms/worker/views.py
@@ -2,53 +2,30 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.forms import modelform_factory
 from website.models import inventory,products,newInventory,user
-
+from worker import function
 def menu(request):
     return render(request,"worker/menu.html")
 
 inventory_form=modelform_factory(inventory,exclude=['available'])
 
-def new_inventory(request):
-    msg=''
+def inventory_receipt(request):
     if request.method == "POST" :
         form=inventory_form(request.POST) 
         if form.is_valid():
-            result=addNewInv(request.POST.dict())
-            if result==0:
-                form.save()
-                inv=inventory.objects.get(available=-1)
-                inv.available=inv.amount
-                inv.save()
-                form.clean()
-                msg="Inventory created succsessfully"
-                return render(request,"worker/new_inventory.html",{"form":form,'message':msg})
-            elif result==1:
-                msg="Item with serial number"
-                return render(request,"worker/new_inventory.html",{"form":form,'message':msg})
-            elif result==2:
-                msg="Inventory created succsessfully"
-                return render(request,"worker/new_inventory.html",{"form":form,'message':msg})
+            try:
+                msg=function.addNewInv(request.POST.dict(),form,user.objects.get(username=request.COOKIES['user']))
+            except:
+                msg2="Item with serial number"
+                return render(request,"worker/new_inventory.html",{"form":form,"message2":msg2})
+
+            return render(request,"worker/new_inventory.html",{"form":form,"message":msg})
         else:
             return render(request,"worker/new_inventory.html",{"form":form})
     else:
         form=inventory_form()
         return render(request,"worker/new_inventory.html",{"form":form})
     
-def addNewInv(form):
-    product=products.objects.get(sku=int(form['sku']))
-    if product.serial_item == 1 :
-        if form['serial']!='' and int(form['amount'])==1:
-            return 0
-        else:
-            return 1
-    inv=inventory.objects.filter(sku=form['sku']).filter(location=form['location'])
-    if len(inv)!=0:
-        inv=inv.first()
-        inv.amount+=int(form['amount'])
-        inv.available+=int(form['amount'])
-        inv.save()
-        return 2
-    
-    return 0
+
+
 
 
