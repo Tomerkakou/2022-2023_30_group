020bbf92ed91335c6d9fd9f1d5b923328fd1cbb4
osnatshabtay <osnatshabtay98@gmail.com>
2023-01-04 21:04:24 +0200

    unit test -> orders

--- wms/student/tests.py
+++ wms/student/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from website.models import products,inventory,locations,user1,specific_order,orders
+from student import function
+from django.contrib.auth.models import Group
 
-# Create your tests here.
+
+class TestWorker_function(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        g=Group.objects.create(name='student')
+        user = user1.objects.create(username= 'matan',email='matan123@gmail.com', full_name = 'matani',role = g)
+        for i in range(10):
+            orders.objects.create(order_number = i+1*400,user_id = user)
+            
+    def test_status_order(self):
+        with self.subTest("Without fillter"):
+            user = user1.objects.get(username= 'matan')
+            self.assertEqual(len(function.getOrders({'order_number':"",'create_date':"",'create_date_end':"",'status':""},user)),10)
