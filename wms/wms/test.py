d26a6c4e7e0d7b6a6ea5148534a0390dbca172da
talboaron <talboaron00@gmail.com>
2023-01-07 21:56:45 +0200

    import fix

--- wms/wms/test.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="rendit-db.crfj8gkk0yln.eu-central-1.rds.amazonaws.com",
-  user="tomer",
-  password="t38tTNsQg2PBltlg"
-)
-
-print(mydb)
--- wms/wms/urls.py
+++ wms/wms/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
-import debug_toolbar
 from website.views import start
 
 urlpatterns = [
@@ -24,6 +23,6 @@ urlpatterns = [
     path('',include('manager.urls')),
     path('',include('worker.urls')),
     path('',include('student.urls')),
-    path('__debug__/',include(debug_toolbar.urls))
+
     
 ]
